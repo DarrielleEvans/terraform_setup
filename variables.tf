72bfa93dc13df49dcbcbb0d634a4b0f32cb76ab9
Darrielle Evans <89504317+DarrielleEvans@users.noreply.github.com>
2023-10-11 23:40:27 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "instance_type" {}
+variable "access_key" {}
+variable "secret_key" {}
+variable "ami" {}
+variable "instance_name" {}
+variable "subnet_id" {}
+variable "key_name" {}
+variable "vpc_security_group_ids" {}
